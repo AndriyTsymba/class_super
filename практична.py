b3b8e9d43ca134c9702efed67fc8ce65963352e1
student <a.tsymbaliuk2007@gmail.com>
2023-05-11 20:51:17 +0300

    дописати практичну закінчив на в класі Animal реалізуйте методи grow, eat, play. Використовуйте рандомайзер для покращення/погіршення показників життя тварини

--- практична.py
+++ практична.py
@@ -1,3 +1,50 @@
 #1
-class Animal
-    def __init__(self, , ):
+import  random
+class Animal:
+    def __init__(self,species,name, age, health, hunger, happiness):
+        super().__init__()
+        self.species = species
+        self.name = name
+        self.age = age
+        self.health = health
+        self.hunger = hunger
+        self.happiness = happiness
+
+    def  grow(self):
+        self.age += 5
+        self.health = random.randint(0,10)
+        self.hunger = random.randint(0,10)
+        self.happiness = random.randint(0,10)
+    def eat(self):
+        if self.health >= 6:
+            self.health +=  random.randint(0,5)
+            self.happiness +=  random.randint(0,5)
+            self.hunger +=  random.randint(0,5)
+        else:
+
+
+            print(f"{self.name},not hungry")
+    def play(self):
+        pass
+
+
+
+
+
+
+
+
+class Zoo(Animal):
+    def __init__(self,species,name, age, health, hunger, happiness,animals):
+        super().__init__(species,name, age, health, hunger, happiness)
+        self.animals = []
+
+
+
+
+
+
+
+
+
+
