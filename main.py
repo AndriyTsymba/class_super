de0011dde4b2d60d7ed663724e97e4f92c254096
student <a.tsymbaliuk2007@gmail.com>
2023-05-11 20:25:35 +0300

    2 приклвд з super() та with open

--- main.py
+++ main.py
@@ -18,3 +18,19 @@ print(dog.breed)
 dog.eat()
 dog.bark()
 #2
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f"name{self.name},age {self.age}"
+class Student(Person):
+    def __init__(self,name,age,student_id):
+      super().__init__(name,age)
+      self.student_id = student_id
+    def __str__(self):
+        return super().__str__() + f" student_id {self.student_id}"
+student1 = Student('Andriy',21,45453)
+print(student1)
+with open("info_student.txt","w") as file:
+    file.write(str(student1))
---
+++ практична.py
@@ -0,0 +1,3 @@
+#1
+class Animal
+    def __init__(self, , ):
